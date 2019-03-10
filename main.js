78a7e6bbca2b3fde16f6eb35296f7357087d487f
gmathe02 <george.mathew@tufts.edu>
2019-03-09 19:06:52 -0500

    draft 1 done

--- main.js
+++ main.js
@@ -6,3 +6,30 @@ function myFunction() {
         x.className = "nav";
     }
 }
+
+var slideIndex = [1, 1];
+var slideId = ["mySlides1", "mySlides2"]
+showDivs(1, 1);
+showDivs(1, 0);
+
+
+
+function plusDivs(n, no) {
+    showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+    var i;
+    var x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) {slideIndex[no] = 1}
+    if (n < 1) {slideIndex[no] = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex[no] - 1].style.display = "block";
+
+    if (no == 2) {
+        var y = document.getElementsByClassName("pic1");
+        y.style.display = "block";
+    }
+}
